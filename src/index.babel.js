f4b5b59affe1a6aa9ad67e2b70aa2672d01d400c
kelevra82 <tdmatte82@gmail.com>
2025-01-23 20:55:45 -0600

    added files and folder structure

---
+++ src/index.babel.js
@@ -0,0 +1,4 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+const root = createRoot(window.bodyTag);
+root.render(/*#__PURE__*/React.createElement("main", null, /*#__PURE__*/React.createElement("h1", null, "Hello world!"), /*#__PURE__*/React.createElement("p", null, "Command is less tedious now.")));
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+const root = createRoot( window.bodyTag );
+root.render(
+    <main>
+        <h1>Transform Your Home with AI</h1>
+        <p>              Experience the future of smart living with our advanced AI-powered home assistant.
+            Control your entire home with just your voice.</p>
+    </main>
+);
