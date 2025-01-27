75688fc6e9f597ececc7a88771eac41606388917
kelevra82 <tdmatte82@gmail.com>
2025-01-27 07:53:37 -0600

    updated files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
 
-const root = createRoot( window.bodyTag );
-root.render(
-    <main>
-        <h1>Transform Your Home with AI</h1>
-        <p>              Experience the future of smart living with our advanced AI-powered home assistant.
-            Control your entire home with just your voice.</p>
-    </main>
-);
+const root = createRoot(window.bodyTag);
+root.render(<main></main>);
--- src/utils/output.js
+++ src/utils/output.js
@@ -1,6 +1,6 @@
-export function outputModule(message = "", outputTag = "outputTag", shouldAppend = true) {
+export function output( message = "", outputTag = "outputTag", shouldAppend = true ) {
     debugger;
 
-    if (shouldAppend) window[outputTag].innerHTML += message;
+    if ( shouldAppend ) window[outputTag].innerHTML += message;
     else window[outputTag].innerHTML = message;
 }
